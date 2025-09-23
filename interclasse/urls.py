980a15d3410df3990e0f913dfb206606226d1ccb
Silas-Prog <152338397+Silas-Prog@users.noreply.github.com>
2025-09-23 07:37:02 -0300

    🔥 Remove code django i18n

--- interclasse/urls.py
+++ interclasse/urls.py
@@ -3,17 +3,11 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.utils.translation import gettext_lazy as _
-from django.conf.urls.i18n import i18n_patterns
 
 urlpatterns = [
-    path('i18n/', include('django.conf.urls.i18n')),
-]
-
-# Rotas com suporte a idiomas
-urlpatterns += i18n_patterns(
     path('admin/', admin.site.urls),
     path('', include('app.urls')),  
-)
+]
 
 handler403 = 'app.views.erro_403_customizado'
 handler404 = 'app.views.erro_404_customizado'
